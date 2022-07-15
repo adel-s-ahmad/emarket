ffddc0a1bcc81c73964425dbde04ca51dbe756e5
Adel Soliman Abdeldayem -X (aabdelda - SigmaIT at Cisco) <aabdelda@cisco.com>
2022-07-15 11:32:07 +0200

    make h2 accessible

--- product/src/main/java/com/emarket/product/ProductApplication.java
+++ product/src/main/java/com/emarket/product/ProductApplication.java
@@ -1,13 +1,11 @@
 package com.emarket.product;
 
 import java.sql.SQLException;
+import org.h2.tools.Server;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import com.emarket.product.services.ProductsService;
 
 @SpringBootApplication
 public class ProductApplication {
@@ -16,10 +14,10 @@ public class ProductApplication {
 		SpringApplication.run(ProductApplication.class, args);
 	}
 	
-//	@Bean(initMethod = "start", destroyMethod = "stop")
-//	public Server inMemoryH2DatabaseaServer() throws SQLException {
-//	    return Server.createTcpServer(
-//	      "-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
-//	}
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server inMemoryH2DatabaseaServer() throws SQLException {
+	    return Server.createTcpServer(
+	      "-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+	}
 
 }
