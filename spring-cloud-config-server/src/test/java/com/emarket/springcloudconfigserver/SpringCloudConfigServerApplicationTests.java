e438aaf4b79bd23d5f13df10108f5c6f3db27ee4
Adel Soliman Abdeldayem -X (aabdelda - SigmaIT at Cisco) <aabdelda@cisco.com>
2022-08-02 23:21:08 +0200

    in process of adding cloud config server

---
+++ spring-cloud-config-server/src/test/java/com/emarket/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emarket.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
